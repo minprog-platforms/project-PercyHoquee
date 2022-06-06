aabca797d9d67af5eea5f58c9ddf24693aabd208
Percy Hoquee <percyhoquee@gmail.com>
2022-06-06 13:34:24 +0200

    Revert "files with history"
    This reverts commit 51a3030276a57b1ecef5fb3e1bd186f6d5d5ecb3.

---
+++ food_tracker/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+
+from .models import Favs, User, Product, Meal, Diary, DiaryEntry, Breakfast, Lunch, Dinner, Snacks, ProductInstance, Favs, Default, DefaultInstance, Faulty
+
+# Register your models here.
+admin.site.register(User)
+admin.site.register(Product)
+admin.site.register(Meal)
+admin.site.register(Diary)
+admin.site.register(DiaryEntry)
+admin.site.register(Breakfast)
+admin.site.register(Lunch)
+admin.site.register(Dinner)
+admin.site.register(Snacks)
+admin.site.register(ProductInstance)
+admin.site.register(Favs)
+admin.site.register(DefaultInstance)
+admin.site.register(Default)
+admin.site.register(Faulty)
